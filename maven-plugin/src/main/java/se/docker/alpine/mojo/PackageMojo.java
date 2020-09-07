c9830d1fd9e26de92f4381cea8f4df479cd0687d
ulfhaga <ulf.haga@gmail.com>
2020-09-07 15:18:07 +0200

    Saving

--- maven-plugin/src/main/java/se/docker/alpine/mojo/PackageMojo.java
+++ maven-plugin/src/main/java/se/docker/alpine/mojo/PackageMojo.java
@@ -3,6 +3,12 @@ package se.docker.alpine.mojo;
 import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import se.docker.alpine.gateway.Client;
+import se.docker.alpine.gateway.ClientDto;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Mojo(name = "create")
 public class PackageMojo implements org.apache.maven.plugin.Mojo
@@ -12,12 +18,41 @@ public class PackageMojo implements org.apache.maven.plugin.Mojo
     @Parameter(property = "source", required = true)
     private String source;
 
+    @Parameter(property = "name", required = true)
+    private String name;
+
+
     @Override
     public void execute()
     {
-        System.out.println("YYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYYY");
-        log.info("Hello, Ulf's world");
         log.info("Source:" + source);
+        log.info("Name:" + name);
+
+
+        Client client = new Client();
+        ClientDto clientDto = getClientDto();
+        try
+        {
+            client.send(clientDto);
+        }
+        catch (IOException e)
+        {
+            log.error(e.getMessage());
+            e.printStackTrace();
+        }
+
+
+    }
+
+    private ClientDto getClientDto()
+    {
+        ClientDto clientDto = new ClientDto();
+        clientDto.setName(name);
+
+        Path sourceDirectory = Paths.get(source);
+        clientDto.setSource(sourceDirectory);
+
+        return clientDto;
     }
 
     @Override
